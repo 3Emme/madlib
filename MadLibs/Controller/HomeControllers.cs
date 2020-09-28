61d79e858c18b424e4694cb3cd3ed6966e62b456
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-09-28 10:53:50 -0700

    made the form.cshtml doc and generated the form
    co-authored-by: Emme Buentiempo <emmettbuentiempo@gmail.com>

---
+++ MadLibs/Controller/HomeControllers.cs
@@ -0,0 +1,27 @@
+using MadLibs.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MadLibs.Controllers {
+    public class HomeController : Controller {
+        [Route ("/form")]
+        public ActionResult Form () { return View (); }
+
+        [Route ("/MadLibResult")]
+        public ActionResult MadlibResult (string noun1, string noun2,string noun3, string adjective1,string adjective2, string adjective3,string adjective4, string verbPastTense1,string verbPastTense2, string verb, string adverb1, string adverb2) {
+            MadLibVariable myMadLibVariable = new MadLibVariable ();
+            myMadLibVariable.Noun1 = noun1;
+            myMadLibVariable.Noun2 = noun2;
+            myMadLibVariable.Noun3 = noun3;
+            myMadLibVariable.Adjective1 = adjective1;
+            myMadLibVariable.Adjective2 = adjective2;
+            myMadLibVariable.Adjective3 = adjective3;
+            myMadLibVariable.Adjective4 = adjective4;
+            myMadLibVariable.VerbPastTense1 = verbPastTense1;
+            myMadLibVariable.VerbPastTense2 = verbPastTense2;
+            myMadLibVariable.Verb = verb;
+            myMadLibVariable.Adverb1 = adverb1;
+            myMadLibVariable.Adverb2 = adverb2;
+            return View (myMadLibVariable);
+        }
+    }
+}
---
+++ MadLibs/Models/MadLibVariable.cs
@@ -0,0 +1,21 @@
+namespace MadLibs.Models
+{
+    public class MadLibVariable
+    {
+        public string Noun1 { get; set;}
+        public string Noun2 { get; set;}
+        public string Noun3 { get; set;}
+        public string Adverb1 { get; set;}
+        public string Adverb2{ get; set;}
+
+        public string Verb { get; set;}
+        public string VerbPastTense1 {get;set;}
+
+        public string VerbPastTense2 {get;set;}
+        public string Adjective1{ get; set;}
+        public string Adjective2{ get; set;}
+        public string Adjective3{ get; set;}
+        public string Adjective4{ get; set;}
+        
+    }
+}
